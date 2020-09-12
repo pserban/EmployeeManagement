a777961f07cde2c7eab096e4772b4356bf07f155
Serban Patriciu <serban_patriciu@hotmail.com>
2020-09-12 09:22:27 -0400

    Lectura 17 - Cod asociat

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -24,7 +24,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddMvcCore(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
