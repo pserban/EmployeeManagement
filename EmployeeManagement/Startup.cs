d5bee329126b570b372449cfe36369a5b4c5dc78
Serban Patriciu <serban_patriciu@hotmail.com>
2020-09-14 12:14:59 -0400

    Lectura 20 - partea 01

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -25,7 +25,9 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services
+                .AddMvc(options => options.EnableEndpointRouting = false)
+                .AddXmlDataContractSerializerFormatters();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
