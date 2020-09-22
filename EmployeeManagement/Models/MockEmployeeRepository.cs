3c95c0cfe98d3de9f827d3961eae026b2d4f89f7
Serban Patriciu <serban_patriciu@hotmail.com>
2020-09-22 10:11:13 -0400

    Lectura 40 - cod asociat

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -13,9 +13,9 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
             {
-                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@pragim.com" },
-                new Employee() { Id = 2, Name = "John", Department = "IT", Email = "john@pragim.com" },
-                new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "sam@pragim.com" }
+                new Employee() { Id = 1, Name = "Mary", Department = Dept.HR, Email = "mary@pragim.com" },
+                new Employee() { Id = 2, Name = "John", Department = Dept.IT, Email = "john@pragim.com" },
+                new Employee() { Id = 3, Name = "Sam", Department = Dept.IT, Email = "sam@pragim.com" }
             };
         }
         public Employee GetEmployee(int Id)
