433a150125ea231e5ed5c496ce880eeb98720ecc
Serban Patriciu <serban_patriciu@hotmail.com>
2020-09-23 08:34:25 -0400

    Lectura 44 - cod asociat B

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -28,7 +28,7 @@ namespace EmployeeManagement
             services
                 .AddMvc(options => options.EnableEndpointRouting = false)
                 .AddXmlSerializerFormatters();
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
