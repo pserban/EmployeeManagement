085cded338c2e8000c8163606a116c4e7c7bf4e6
Serban Patriciu <serban_patriciu@hotmail.com>
2020-10-02 07:22:10 -0400

    Lectura 60 - cod asociat

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -43,6 +43,7 @@ namespace EmployeeManagement
             }
             else
             {
+                app.UseExceptionHandler("/Error");
                 app.UseStatusCodePagesWithReExecute("/Error/{0}");
             }
 
