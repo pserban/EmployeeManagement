bf108eff08f8e29e4262fa7338f494492d73aa80
Serban Patriciu <serban_patriciu@hotmail.com>
2020-10-06 20:10:12 -0400

    Lectura 77 - cod asociat

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -42,7 +42,7 @@ namespace EmployeeManagement
                 })
                 .AddXmlSerializerFormatters();
 
-            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
                 {
                     options.Password.RequiredLength = 10;
                     options.Password.RequiredUniqueChars = 3;
