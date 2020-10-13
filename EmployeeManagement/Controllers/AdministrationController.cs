7cfd6dda20f7da14d530a8daa447c29a3bd18126
Serban Patriciu <serban_patriciu@hotmail.com>
2020-10-13 17:16:27 -0400

    Lectura 86 - cod asociat

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -24,6 +24,34 @@ namespace EmployeeManagement.Controllers
             this.userManager = userManager;
         }
         
+
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListUser");
+            }
+        }
+
         [HttpGet]
         public IActionResult ListUsers()
         {
