84ef1e2f5075f0415d20b9e82aab0466be743327
Serban Patriciu <serban_patriciu@hotmail.com>
2020-10-19 17:49:54 -0400

    Lectura 119 - cod asociat

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -73,12 +73,19 @@ namespace EmployeeManagement
                     options.Password.RequireNonAlphanumeric = false;
 
                     options.SignIn.RequireConfirmedEmail = true;
+
+                    options.Tokens.EmailConfirmationTokenProvider = "CustomEmailConfirmation";
                 })
                 .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
+                .AddDefaultTokenProviders()
+                .AddTokenProvider<CustomEmailConfirmationTokenProvider
+                                        <ApplicationUser>>("CustomEmailConfirmation"); ;
 
             services.Configure<DataProtectionTokenProviderOptions>(o =>
                     o.TokenLifespan = TimeSpan.FromHours(5));
+
+            services.Configure<CustomEmailConfirmationTokenProviderOptions>(o =>
+                    o.TokenLifespan = TimeSpan.FromDays(3));
 
             services.AddAuthorization(options =>
             {
